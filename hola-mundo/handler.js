551528162601ceead4ef622e144262fc14900235
Daniel Cardozo C <daniel.cardozo0621@gmail.com>
2020-09-20 22:37:15 -0500

    Hola mundo tutotial added

---
+++ hola-mundo/handler.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const queryString = require('querystring');
+
+module.exports.hello = async event => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: `Hola ${event.pathParameters.name}`,
+            input: event.name,
+        }, null, 2),
+    };
+};
+
+module.exports.showUser = async event => {
+    const body = queryString.parse(event['body']);
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Petición POST',
+            input: `Hola ${body.name} ${body.lastname}`,
+        }, null, 2),
+    };
+};
